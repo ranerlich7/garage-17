5b383abb2319cd2d6bdd822b1a178b69e24457d7
Ran JB <ranerlich@gmail.com>
2024-11-29 09:23:38 +0200

    added app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import "./App.css"
 
 function App() {
+  const [cars, setCars] = useState(["Honda", "Toyota", "Tesla"])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {cars.map((car, index) => (
+        <h1 key={index}>{car}</h1>
+      ))}
+    </>
+  )
 }
 
-export default App;
+export default App

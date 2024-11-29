5fda8f912c88491e4f4b272db0ba4d58b27b6982
Ran JB <ranerlich@gmail.com>
2024-11-29 10:00:52 +0200

    added car list

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import { useState } from "react"
 import "./App.css"
 
 function App() {
-  const [cars, setCars] = useState(["Honda", "Toyota", "Tesla"])
+  const [cars, setCars] = useState([
+    { name: "Honda", number: "12-412-56" },
+    { name: "Tesla", number: "13-246-63" },
+  ])
 
   return (
     <>
       {cars.map((car, index) => (
-        <h1 key={index}>{car}</h1>
+        <div className="card" key={index}>
+          <div className="card-title">
+            <h3>{car.name.toUpperCase()}</h3>
+            <div className="car-button">{car.number}</div>
+          </div>
+          <div className="card-action">
+            <button>Click</button>
+          </div>
+        </div>
       ))}
     </>
   )

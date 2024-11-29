fe86a6e4d7a887418c913e3f8e91fffb4db03b8f
Ran JB <ranerlich@gmail.com>
2024-11-29 10:39:10 +0200

    add car form added

--- src/App.js
+++ src/App.js
@@ -6,13 +6,21 @@ function App() {
     { name: "Honda", number: "12-412-56" },
     { name: "Tesla", number: "13-246-63" },
   ])
+  const [carName, setCarName] = useState("")
+  const [carNumber, setCarNumber] = useState("")
 
+  function addCar() {
+    const newCar = { name: carName, number: carNumber }    
+    setCars([...cars, newCar])        // spread operator example 
+    setCarName("")
+    setCarNumber("")
+  }
   return (
     <>
       {cars.map((car, index) => (
         <div className="card" key={index}>
           <div className="card-title">
-            <h3>{car.name.toUpperCase()}</h3>
+            <h3>{car.name}</h3>
             <div className="car-button">{car.number}</div>
           </div>
           <div className="card-action">
@@ -20,6 +28,18 @@ function App() {
           </div>
         </div>
       ))}
+      <div>
+        Name:
+        <input value={carName} onChange={(e) => setCarName(e.target.value)} />
+        <br></br>
+        Number:
+        <input
+          value={carNumber}
+          onChange={(e) => setCarNumber(e.target.value)}
+        />
+        <br />
+        <button onClick={addCar}>Add Car</button>
+      </div>
     </>
   )
 }
